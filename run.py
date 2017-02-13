c5924a82aa7e6ffd8d8b12f96b81c084c871d799
armavrdsp <kun_xs@126.com>
2017-02-13 17:48:54 +0800

    Update run.py
    add run Network2

--- run.py
+++ run.py
@@ -6,8 +6,19 @@ Created on Mon Feb 06 20:40:34 2017
 """
 
 import Network
+import Network2
 import mnist_loader
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = Network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = Network2.Network2([784, 30, 10], cost=Network2.CrossEntropyCost)
+evaluation_cost, evaluation_accuracy, \
+training_cost, training_accuracy = net.SGD(training_data, 30, 10, 0.5, lmbda = 5.0, \
+                                           evaluation_data = validation_data, \
+                                           monitor_evaluation_accuracy = True, \
+                                           monitor_evaluation_cost = True, \
+                                           monitor_training_accuracy = True, \
+                                           monitor_training_cost = True)
